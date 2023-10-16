b3e2cf09e987344b7993a7a81c712dfd6872264a
OxygenAddicted <alimamul.luthfi@gmail.com>
2023-10-16 18:29:06 +0700

    Fixing some bugs

--- index.js
+++ index.js
@@ -59,8 +59,8 @@ client.on('interactionCreate', async interaction => {
       await client.commands.get(commandName).execute(interaction);
     } catch (error) {
       console.error(error);
-      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      await interaction.reply({ content: 'Mager ngejawab pertanyaanmu', ephemeral: true });
     }
   });
   
-  client.login(token);
+client.login(token);
